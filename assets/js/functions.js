791401defea68f6d196d41cdfe84edfb957fe99e
”Ozi <31310132+OziOcb@users.noreply.github.com>
2018-06-08 12:58:09 +0100

    Add full navigation + Major Assets update
    The navigation has been completed
    Added all the necessary assets

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,1 +1,7 @@
-$(function(){});
+
+// Add 'active' class to the Toggler
+const toggler = document.getElementById('main-navi-toggler');
+
+toggler.addEventListener('click', function() {
+  toggler.classList.toggle('toggler--active')
+}, false)

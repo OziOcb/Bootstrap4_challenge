8c424bbe4e1b68bc90ded2e65fb8e15c31973c08
”Ozi <31310132+OziOcb@users.noreply.github.com>
2018-06-10 11:18:22 +0100

    Make nav-links able to close the main navigation after clicking on them

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,7 +1,20 @@
+/* //────  ──────────────────────────────────────────────────────────────────────────────────
+  MAIN NAVIGATION 
+//────  ──────────────────────────────────────────────────────────────────────────────────*/
 
-// Add 'active' class to the Toggler
+// Adds 'active' class to the Toggler
 const toggler = document.getElementById('main-navi-toggler');
 
 toggler.addEventListener('click', function() {
   toggler.classList.toggle('toggler--active')
 }, false)
+
+
+// Removes .toggle--active class from #main-navi-toggler after clicking on any .nav-link
+const navigationLink = document.querySelectorAll('.nav-link');
+
+for(i = 0; i < navigationLink.length; i++) {
+  navigationLink[i].addEventListener('click', function() {
+    document.getElementById('main-navi-toggler').classList.remove('toggler--active');
+  }, false)
+};

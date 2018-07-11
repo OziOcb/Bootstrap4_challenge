f289edfe26ff714555d3121fcbfe58fda5714940
”Ozi <31310132+OziOcb@users.noreply.github.com>
2018-07-11 14:59:20 +0100

    Change the smooth scrolling code from jquery to vanilla JS

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -22,42 +22,10 @@ for(i = 0; i < navigationLink.length; i++) {
 /* //────  ──────────────────────────────────────────────────────────────────────────────────
   SMOOTH SCROLL
 //────  ──────────────────────────────────────────────────────────────────────────────────*/
-// Select all links with hashes
-$('a[href*="#"]')
-  // Remove links that don't actually link to anything
-  .not('[href="#"]')
-  .not('[href="#0"]')
-  .click(function(event) {
-    // On-page links
-    if (
-      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '')
-      &&
-      location.hostname == this.hostname
-    ) {
-      // Figure out element to scroll to
-      var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-      // Does a scroll target exist?
-      if (target.length) {
-        // Only prevent default if animation is actually gonna happen
-        event.preventDefault();
-        $('html, body').animate({
-          scrollTop: target.offset().top - 60
-        }, 1000, function() {
-          // Callback after animation
-          // Must change focus!
-          var $target = $(target);
-          $target.focus();
-          if ($target.is(":focus")) { // Checking if the target was focused
-            return false;
-          } else {
-            $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
-            $target.focus(); // Set focus again
-          };
-        });
-      }
-    }
-  });
+// Makes that scrolling to the anchor includes the height of the fixed-nav
+var defaultDuration = 777 // ms
+var edgeOffset = 65 // px
+zenscroll.setup(defaultDuration, edgeOffset)
 
 /* //────  ──────────────────────────────────────────────────────────────────────────────────
   RADIAL INDICATORS

f1dd6bb754d27f61de3064abcb504a4adad685ce
”Ozi <31310132+OziOcb@users.noreply.github.com>
2018-07-13 20:23:41 +0100

    Add basic logic for a Projects Section Random Highlight

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -90,3 +90,76 @@ window.addEventListener('scroll', function (event) {
 
 	}
 }, false);
+
+/* //────  ──────────────────────────────────────────────────────────────────────────────────
+  Projects Section Random Highlight
+//────  ────────────────────────────────────────────────────────────────────────────────── */
+// Stworzyc media query dla kazdej szerokosci ekranu
+const mq4 = window.matchMedia( "(min-width: 30em)" );
+const mq5 = window.matchMedia( "(min-width: 40em)" );
+const mq6 = window.matchMedia( "(min-width: 50em)" );
+const mq7 = window.matchMedia( "(min-width: 60em)" );
+
+// Stworzyć funkcje do generowania losowych liczb zaleznie od MQ
+function randomNum(num) {
+  return Math.floor(Math.random() * num) + 1;
+}
+
+// Funkcjia musi
+// Usunac klase projects__item--active z kazdego elementu
+const tiles = document.getElementsByClassName('projects__item');
+function removeClasses() {
+  for (i = 0; i < tiles.length; i++) {
+    tiles[i].classList.remove('projects__item--active')
+  }
+}
+removeClasses();
+
+
+// Wygenerować losową liczbe od 1 do 4 dla poziomu
+// Wziasc pod uwage to ze tylko na najmiejszym MQ sa 4 linie kafelkow
+function randomHorizontalLine() {
+  let hori;
+  if (mq4.matches) {
+    hori = randomNum(3);
+  } else {
+    hori = randomNum(4);
+  }
+  return hori;
+};
+
+// Wygenerować losową liczbe od 1 do 3 dla pionu
+// Wziasc pod uwage to ze z kazdym kolejnym MQ liczba kafelkow zwieksza sie o 1
+function randomVerticalLine() {
+  let verti;
+  if (mq7.matches) {
+    verti = randomNum(7)
+  } else if (mq6.matches) {
+    verti = randomNum(6)
+  } else if (mq5.matches) {
+    verti = randomNum(5)
+  } else if (mq4.matches) {
+    verti = randomNum(4)
+  } else {
+    verti = randomNum(3)
+  }
+  return verti;
+}
+
+// Dodac klase --active do losowo wygenerowanego kafelka
+const tile = document.getElementsByClassName(`projects__item--${randomHorizontalLine()}-${randomVerticalLine()}`)[0];
+function highlightRandomTile() {
+  tile.classList.add('projects__item--active');
+}
+highlightRandomTile();
+
+
+
+console.log(tile);
+
+
+
+
+// Do odpowieniego elementu (np. .projects__item--1-1) dodać klase projects__item--active
+
+// Zapętlić cala ta funkcjie 

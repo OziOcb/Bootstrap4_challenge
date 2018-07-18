f0c0e3c20326f2ff744026225d74172274517d48
”Ozi <31310132+OziOcb@users.noreply.github.com>
2018-07-18 10:11:15 +0100

    Improve js for Logos Section Random Highlight and Radial Indicators

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -30,7 +30,7 @@ zenscroll.setup(defaultDuration, edgeOffset)
 /* //────  ──────────────────────────────────────────────────────────────────────────────────
   RADIAL INDICATORS
 //────  ────────────────────────────────────────────────────────────────────────────────── */
-var radialObj1 = radialIndicator('#indicatorContainer1', {
+const radialObj1 = radialIndicator('#indicatorContainer1', {
   barColor : '#f34739',
   barBgColor: '#e5e3e3',
   barWidth : 6,
@@ -43,7 +43,7 @@ var radialObj1 = radialIndicator('#indicatorContainer1', {
 
 radialObj1.animate(0);
 
-var radialObj2 = radialIndicator('#indicatorContainer2', {
+const radialObj2 = radialIndicator('#indicatorContainer2', {
   barColor : '#009989',
   barBgColor: '#e5e3e3',
   barWidth : 6,
@@ -56,7 +56,7 @@ var radialObj2 = radialIndicator('#indicatorContainer2', {
 
 radialObj2.animate(0);
 
-var radialObj3 = radialIndicator('#indicatorContainer3', {
+const radialObj3 = radialIndicator('#indicatorContainer3', {
   barColor : '#152b3c',
   barBgColor: '#e5e3e3',
   barWidth : 6,
@@ -70,8 +70,8 @@ var radialObj3 = radialIndicator('#indicatorContainer3', {
 radialObj3.animate(0);
 
 // get the #growth section element
-var isInViewport = function (elem) {
-  var bounding = elem.getBoundingClientRect();
+const isInViewport = elem => {
+  const bounding = elem.getBoundingClientRect();
   return (
       bounding.top >= 0 &&
       bounding.left >= 0 &&
@@ -80,9 +80,9 @@ var isInViewport = function (elem) {
   );
 };
 
-var growth = document.querySelector('.indicator');
+const growthSection = document.querySelector('.indicator');
 window.addEventListener('scroll', function (event) {
-	if (isInViewport(growth)) {
+	if (isInViewport(growthSection)) {
 
     radialObj1.animate(50);
     radialObj2.animate(35);
@@ -92,7 +92,7 @@ window.addEventListener('scroll', function (event) {
 }, false);
 
 /* //────  ──────────────────────────────────────────────────────────────────────────────────
-  Projects Section Random Highlight
+  Projects and Logos Sections Random Highlight
 //────  ────────────────────────────────────────────────────────────────────────────────── */
 // BASE
 // Stworzyc media query dla kazdej szerokosci ekranu
@@ -106,32 +106,32 @@ const randomNum = num => Math.floor(Math.random() * num) + 1;
 
 // Wygenerować losową liczbe od 1 do 4 dla poziomu
 // Wziac pod uwage to ze tylko na najmiejszym MQ sa 4 linie kafelkow
-const randomHorizontalLine = () => {
-  let hori;
-  if (mq4.matches) {
-    hori = randomNum(3);
+const randomHorizontalLine = section => {
+  if (section === 'tiles') {
+    if (mq4.matches) {
+      return randomNum(3);
+    } else {
+      return randomNum(4);
+    }
   } else {
-    hori = randomNum(4);
+    return randomNum(4);
   }
-  return hori;
 };
 
 // Wygenerować losową liczbe od 1 do 3 dla pionu
 // Wziac pod uwage to ze z kazdym kolejnym MQ liczba kafelkow zwieksza sie o 1
 const randomVerticalLine = () => {
-  let verti;
   if (mq7.matches) {
-    verti = randomNum(7)
+    return randomNum(7)
   } else if (mq6.matches) {
-    verti = randomNum(6)
+    return randomNum(6)
   } else if (mq5.matches) {
-    verti = randomNum(5)
+    return randomNum(5)
   } else if (mq4.matches) {
-    verti = randomNum(4)
+    return randomNum(4)
   } else {
-    verti = randomNum(3)
+    return randomNum(3)
   }
-  return verti;
 };
 
 // Funkcjia musi
@@ -155,7 +155,7 @@ const removeClasses = (el) => {
 
 
 // Stworzyc timer oraz funkcje ktora go zatrzymuje
-let timer = setInterval(randHighlight, 4000);
+let timer = setInterval(randomHighlight, 4000);
 
 const stopTimer = () => {
   clearInterval(timer);
@@ -169,7 +169,7 @@ const stopTimer = () => {
 // funkacja ktora wybiera losowy kafelek
 const randomTile = () => {
   // Wybiera losowy kafelek
-  const tile = document.getElementsByClassName(`projects__item--${randomHorizontalLine()}-${randomVerticalLine()}`)[0];
+  const tile = document.getElementsByClassName(`projects__item--${randomHorizontalLine('tiles')}-${randomVerticalLine()}`)[0];
 
   // Dodaje klase --active do losowo wygenerowanego kafelka
   tile.classList.add('projects__item--active');
@@ -179,17 +179,9 @@ const randomTile = () => {
 
 // funkacja ktora wybiera losowe logo
 // losowa liczba dla logos
-
-// !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-// UWAGA POTRZEBNA ZMIANA!! LICZBY MUSZA GENEROWAC SIE ZALEZZNIE OD SZEROKOSCI OKNA
-// W TEJ CHWILI TAK NIE JEST! CZYLI NA MNIEJSZYCH SZEROKOSCIACH MOZE ZOSTAC 
-// POSDWIETLONE LOGO KTORE JEST NIEWIDOCZNE
-// !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-const randomLogoNum = () => Math.floor(Math.random() * logos.length);
-
 const randomLogo = () => {
   // Wybiera losowe logo
-  const logo = document.getElementsByClassName('logos__item')[randomLogoNum()];
+  const logo = document.getElementsByClassName(`logos__item--${randomHorizontalLine('logos')}-${randomVerticalLine()}`)[0];
 
   // Dodaje klase --active do losowo wygenerowanego logo
   logo.classList.add('logos__item--active');
@@ -198,7 +190,7 @@ const randomLogo = () => {
 
 
 // funkcja ktora podswietla losowy kalfelek lub logo
-function randHighlight() {
+function randomHighlight() {
   // Usówa klase --active z kadego kafelka
   removeClasses(tiles);
   randomTile();
@@ -230,8 +222,8 @@ const events = section => {
   // resetuje interwal
   const resetInter = time => {
     stopTimer();
-    timer = setInterval(randHighlight, time);
-    randHighlight();
+    timer = setInterval(randomHighlight, time);
+    randomHighlight();
   };
 
   // odpala ponownie losowe podswietlanie kafleka, po opuszczeiu kursora
